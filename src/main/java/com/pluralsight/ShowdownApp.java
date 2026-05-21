c45f1fed98e6a06ad6f23db9be313d5c6b2d5ef4
edomg <edomgezahegn54@gmail.com>
2026-05-21 15:04:36 -0500

    Instantiate superhero and villain characters

--- src/main/java/com/pluralsight/ShowdownApp.java
+++ src/main/java/com/pluralsight/ShowdownApp.java
@@ -2,7 +2,12 @@ package com.pluralsight;
 
 public class ShowdownApp {
     public static void main(String[] args) {
+        SuperHero hero = new SuperHero("Spider Man", 100, 20);
+        SuperVillain villain = new SuperVillain("Green Goblin", 100, 15);
+
+        System.out.println("The showdown begins!");
+    }
 
     }
-}
+
 
