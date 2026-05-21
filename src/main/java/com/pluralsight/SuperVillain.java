64fa54ff6dda98321c92e4a5fbf9639b2281a65a
edomg <edomgezahegn54@gmail.com>
2026-05-21 14:57:47 -0500

    Create SuperVillain class using inheritance

---
+++ src/main/java/com/pluralsight/SuperVillain.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class SuperVillain extends Person {
+    public SuperVillain(String name, int health, int attackPower) {
+        super(name, health, attackPower);
+    }
+
+}
