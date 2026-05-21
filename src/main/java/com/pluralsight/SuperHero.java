c3c923b151f31fddb1ab8b4708aed34e0a7b11fc
edomg <edomgezahegn54@gmail.com>
2026-05-21 14:54:47 -0500

    Create SuperHero class using inheritance

---
+++ src/main/java/com/pluralsight/SuperHero.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class SuperHero extends Person {
+    public SuperHero(String name, int health, int attackPower) {
+        super(name, health, attackPower);
+    }
+}
