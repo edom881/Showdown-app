a2ab5bd93249609d2a517c6ec1f25900fca28239
edomg <edomgezahegn54@gmail.com>
2026-05-21 15:18:58 -0500

    Add battle loop for hero and villain combat

--- src/main/java/com/pluralsight/ShowdownApp.java
+++ src/main/java/com/pluralsight/ShowdownApp.java
@@ -6,8 +6,17 @@ public class ShowdownApp {
         SuperVillain villain = new SuperVillain("Green Goblin", 100, 15);
 
         System.out.println("The showdown begins!");
+        while (hero.isAlive() && villain.isAlive()) {
+
+            hero.attack(villain);
+
+            if (villain.isAlive()) {
+                villain.attack(hero);
+            }
+
+        }
     }
 
-    }
+}
 
 
